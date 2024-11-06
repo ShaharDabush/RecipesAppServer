92f8b19158de6f424968a3966a2ca22f23712c4b
User <User@LAB3-3>
2024-11-06 10:24:40 +0200

    asd

---
+++ RecipesAppServer/DTO/RegisterInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipesAppServer.DTO
+{
+    public class RegisterInfo
+    {
+        public User UserInfo { get; set; }
+        public Storage StorageInfo { get; set; }
+        public string StorageCodeInfo { get; set; }
+    }
+}
